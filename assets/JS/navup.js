b56d278aa5793120cc3f40cccca5dbeabc31af99
Raven <priyadarsi02@gmail.com>
2022-09-13 20:09:24 +0530

    13/09/2022 8:09pm
    made changes in navbar js

--- assets/JS/navup.js
+++ assets/JS/navup.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky",window.scrollY > 0);
+    nav.classList.toggle("sticky",window.scrollY > 50);
 });
