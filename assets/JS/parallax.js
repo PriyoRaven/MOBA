62a5a46119878e66c9425257947878ff2d1811f6
Raven <priyadarsi02@gmail.com>
2022-09-17 15:39:25 +0530

    3:39pm
    changes in parallax

--- assets/JS/parallax.js
+++ assets/JS/parallax.js
@@ -4,8 +4,7 @@ function parallax(e){
         const speed = layer.getAttribute('data-speed')
 
         const x = (window.innerHeight - e.pageX*speed)/100
-        const y = (window.innerWidth - e.pageY*speed)/100
 
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+        layer.style.transform = `translateX(${x}px)`
     })
 }
