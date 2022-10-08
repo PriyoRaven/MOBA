06d7639532aca9afc77d2278cafdb3633468ff75
Raven <priyadarsi02@gmail.com>
2022-10-08 13:53:31 +0530

    1:53pm
    overlay background black done

--- assets/JS/pop.js
+++ assets/JS/pop.js
@@ -1,10 +1,11 @@
 function popupOpen()
 {
     document.getElementById("popup").style.display="block";
-    document.getElementById("overlay").style.display="block";
+    document.getElementById("blurrybg").style.display="block";
+    
 }
 function popupClose()
 {
     document.getElementById("popup").style.display="none";
-    document.getElementById("overlay").style.display="none";
+    document.getElementById("blurrybg").style.display="none";
 }
