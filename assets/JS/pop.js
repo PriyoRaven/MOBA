cc3ffa523cec38677aae5679877ffce105e89740
Raven <priyadarsi02@gmail.com>
2022-10-08 12:13:38 +0530

    12:12pm
    pop-up sign-up/ sign-in overlay done

---
+++ assets/JS/pop.js
@@ -0,0 +1,10 @@
+function popupOpen()
+{
+    document.getElementById("popup").style.display="block";
+    document.getElementById("overlay").style.display="block";
+}
+function popupClose()
+{
+    document.getElementById("popup").style.display="none";
+    document.getElementById("overlay").style.display="none";
+}
